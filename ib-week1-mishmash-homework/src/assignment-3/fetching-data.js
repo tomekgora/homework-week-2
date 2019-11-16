63d4049877858810a84a929b7f02d17a367917d3
tomekgora <tomekgora@gmail.com>
2019-11-16 14:02:28 +0100

    Skywalker and Vader data inserted into separate divs to prevent overwriting, button not working

--- ib-week1-mishmash-homework/src/assignment-3/fetching-data.js
+++ ib-week1-mishmash-homework/src/assignment-3/fetching-data.js
@@ -1,18 +1,19 @@
+// script runs on page load and fetches Skywalker data
 fetch("https://swapi.co/api/people/1/") 
   .then(response => response.json()) 
   .then(myJson => { 
-    document.body.innerHTML =
+    document.getElementById('LukeSkywalker').innerHTML =
       "<pre>" + JSON.stringify(myJson, null, " ") + "</pre>"
   })
 
-// function called on clicking DARTH VADER button
+// function called on clicking DARTH VADER button, fetches Darth Vader data
   function getDarthVader() {
     fetch("https://swapi.co/api/people/4/") 
     .then(response => response.json()) 
     .then(myJson => { 
-      document.body.innerHTML =
+      document.getElementById('DarthVader').innerHTML =
         "<pre>" + JSON.stringify(myJson, null, " ") + "</pre>"
     })
   }
 
-  document.getElementById('buttonVader').addEventListener("click", getDarthVader());
+//   document.getElementById('buttonVader').addEventListener("click", getDarthVader());
