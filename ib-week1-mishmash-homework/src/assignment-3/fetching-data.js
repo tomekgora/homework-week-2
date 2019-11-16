ad13ef1dabfc7f16473d1af1dbfe88df6d5701db
tomekgora <tomekgora@gmail.com>
2019-11-16 13:48:12 +0100

    added fn to fetch DarthVader, added button event listener, load sequence problem

--- ib-week1-mishmash-homework/src/assignment-3/fetching-data.js
+++ ib-week1-mishmash-homework/src/assignment-3/fetching-data.js
@@ -4,3 +4,15 @@ fetch("https://swapi.co/api/people/1/")
     document.body.innerHTML =
       "<pre>" + JSON.stringify(myJson, null, " ") + "</pre>"
   })
+
+// function called on clicking DARTH VADER button
+  function getDarthVader() {
+    fetch("https://swapi.co/api/people/4/") 
+    .then(response => response.json()) 
+    .then(myJson => { 
+      document.body.innerHTML =
+        "<pre>" + JSON.stringify(myJson, null, " ") + "</pre>"
+    })
+  }
+
+  document.getElementById('buttonVader').addEventListener("click", getDarthVader());
