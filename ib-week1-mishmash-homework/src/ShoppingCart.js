902eb7cb954c5d7c28077a732b5e73cef9502e99
tomekgora <tomekgora@gmail.com>
2019-11-16 14:39:42 +0100

    moved assignment1 files to src directory

---
+++ ib-week1-mishmash-homework/src/ShoppingCart.js
@@ -0,0 +1,17 @@
+// Checking my ShoppingCart in shopping-run.js
+
+class ShoppingCart {
+    constructor() {
+
+    }
+
+// Mehofs for the class ShoppingCart
+    getItems(){}
+    addItem(){}
+    clear() {}
+    total() {}
+}
+
+
+
+module.exports = 
