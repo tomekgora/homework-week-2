2124bba62b07d0978db142cbf64561776109f4c4
tomekgora <tomekgora@gmail.com>
2019-11-16 19:45:44 +0100

    groupAdultsByAge sorts filters out age <18, returns empty object if input is empty array

--- ib-week1-mishmash-homework/src/transform.js
+++ ib-week1-mishmash-homework/src/transform.js
@@ -1,14 +1,23 @@
 // use filter to make an array excluding records with age < 18
 // work only with this new array with reduce()
 
-// prepData - data pre-filtered for age < 18
+// at the highest level this function should return an object
+
 
 function groupAdultsByAgeRange(data) { 
+    // prepData - data pre-filtered for age < 18
     prepData = data.filter(
         (record) => {return record.age >18}
     )
+    // start working on prepData array of people > 18
+    // I could sort the array by age first to make processing
+    // easier later on --- maybe later.
 
-    
+    return prepData.reduce(
+        function groupByAgeRange(){
+            prepData.filter()
+        },{}
+    ) 
 }
 
 
